67bed00312a80dfd66adeb1dc1cdf080e19f80f5
Olivier B <olivier.berghmans@one16.be>
2024-03-14 22:43:30 +0100

    Added colour to winning squares

--- ttt-react-dev/src/App.js
+++ ttt-react-dev/src/App.js
@@ -2,39 +2,43 @@ import { useState } from 'react';
 
 /*
  TODO
-  4) When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
+  4) When no one wins, display a message about the result being a draw).
   5) Display the location for each move in the format (row, col) in the move history list.
 */
 
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
+function Square({value, onSquareClick, winner}) {
+    const className = winner
+        ? "square winner"
+        : "square"
+    return (
+        <button className={className} onClick={onSquareClick}>
+          {value}
+        </button>
+    );
 }
 
 function Board({ xIsNext, squares, onPlay }) {
 
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    onPlay(nextSquares);
-  }
+      function handleClick(i) {
+        const [a,b,c,winner]=calculateWinner(squares);
+        if (winner || squares[i]) {
+          return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+          nextSquares[i] = 'X';
+        } else {
+          nextSquares[i] = 'O';
+        }
+        onPlay(nextSquares);
+        }
 
 
-  const winner = calculateWinner(squares);
+        const [coordinateA, coordinateB, coordinateC, winner] = calculateWinner(squares);
         let status;
         if (winner) {
-          status = "Winner: " + winner;
+           status = "Winner: " + winner;
         } else {
           status = "Next player: " + (xIsNext ? "X" : "O");
         }
@@ -46,8 +50,14 @@ function Board({ xIsNext, squares, onPlay }) {
 
     for (let i = 0; i < 3; i++) {
         const col = [];
-        for (let j = 0; j < 3; j++) {//push argument without {}, <>
-            col.push(<Square key={k++} value={squares[3*i+j]} onSquareClick={() => handleClick(3*i+j)} />);
+        for (let j = 0; j < 3; j++) { //push argument without {}, <>
+
+            const gridNumber = 3*i+j
+            let winningCoordinate = false;
+            if([coordinateA, coordinateB, coordinateC].includes(gridNumber)) {
+                winningCoordinate = true;
+            }
+            col.push(<Square key={k++} value={squares[gridNumber]} onSquareClick={() => handleClick(gridNumber)} winner={winningCoordinate} />);
         }
         grid.push(<div key={k} className="board-row">{col}</div>);
     }
@@ -141,8 +151,8 @@ function ToggleButton({flipHistory}) {
   };
 
   return (
-    <button onClick={handleChange} className={`toggle-button ${isToggled ? 'on' : 'off'}`}>
-      {isToggled ? 'RECENT LAST' : 'RECENT FIRST'}
+    <button onClick={handleChange} className={`toggle-button ${isToggled ? 'FIRST' : 'LAST'}`}>
+      {isToggled ? 'CURRENT FIRST' : 'CURRENT LAST'}
     </button>
   );
 }
@@ -161,9 +171,11 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [a, b, c, squares[a]];
+//      debugger;
     }
   }
-  return null;
+//  debugger;
+  return [null, null, null, null];
 }
 
